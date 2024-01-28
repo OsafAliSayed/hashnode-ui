e206735e993cb783ba3d1a70c3647c6e1397006a
Shakir Ali <sayedshakirali0@gmail.com>
2024-01-28 19:07:15 +0530

    Added Routing

--- frontend/hashnode-ui/src/app/signin/signin.component.ts
+++ frontend/hashnode-ui/src/app/signin/signin.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { AppState } from '../states/app.state';
 import { selectLogin } from '../states/signin/signin.selector';
 import { login } from '../states/signin/signin.actions';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -16,13 +17,15 @@ import { login } from '../states/signin/signin.actions';
 export class SigninComponent {
   logged: Observable<number>
 
-  constructor(private store: Store<AppState>){
+  constructor(private store: Store<AppState>, private router: Router){
     this.logged = this.store.select(selectLogin)
   }
+
 
   signin() {
     // Call the login API here and then implement the dispatcher inside to update the state
     this.store.dispatch(login())
+    this.router.navigate(['blog']);
     console.log(this.logged)
   }
 
