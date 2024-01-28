7a09dead03bcab5bdd430409d97d3333f27d6c22
Shakir Ali <sayedshakirali0@gmail.com>
2024-01-28 19:07:15 +0530

    Added Routing

--- frontend/hashnode-ui/src/app/app.component.ts
+++ frontend/hashnode-ui/src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { CommonModule } from '@angular/common';
+import { BlogComponent } from './blog/blog.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SigninComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend/hashnode-ui/src/app/app.routes.ts
+++ frontend/hashnode-ui/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
+import { BlogComponent } from './blog/blog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {'path': 'signin', 'title': 'Sign In', component:SigninComponent},
+    {'path': 'blog', 'title': 'Blog', component:BlogComponent}    
+];
