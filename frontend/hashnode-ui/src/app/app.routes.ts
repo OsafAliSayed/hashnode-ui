88cb19af7438b38e84fef72a72c433630ab09898
osafalisayed <aousafalisayedb2@gmail.com>
2024-01-30 23:48:07 +0530

    Added dashboard component

--- frontend/hashnode-ui/src/app/app.routes.ts
+++ frontend/hashnode-ui/src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { BlogComponent } from './blog/blog.component';
-
+import { DashboardComponent } from './dashboard/dashboard.component';
 export const routes: Routes = [
     {'path': 'signin', 'title': 'Sign In', component:SigninComponent},
-    {'path': 'blog', 'title': 'Blog', component:BlogComponent}    
+    {'path': 'blog', 'title': 'Blog', component:BlogComponent} ,
+    {'path': 'dashboard', 'title': 'Dashboard', component:DashboardComponent}    
 ];
