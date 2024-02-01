150fdc93c2e6052c1e27b2f20e0844a8ff5d5262
osafalisayed <aousafalisayedb2@gmail.com>
2024-02-02 00:18:05 +0530

    Editor Implementation

--- frontend/hashnode-ui/src/app/app.component.ts
+++ frontend/hashnode-ui/src/app/app.component.ts
@@ -4,11 +4,11 @@ import { SigninComponent } from './signin/signin.component';
 import { CommonModule } from '@angular/common';
 import { BlogComponent } from './blog/blog.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { EditorModule } from '@tinymce/tinymce-angular';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, EditorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
